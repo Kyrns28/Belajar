8e3e684c5d324885a1de8033ac9f3109ebf911ad
Kyrns28 <123502414+Kyrns28@users.noreply.github.com>
2026-01-21 21:55:01 +0700

    update

--- Ngoprek/Golang/operator.go
+++ Ngoprek/Golang/operator.go
@@ -7,4 +7,13 @@ import (
 func operators() {
 	var a = 15 + 25
 	fmt.Println(a)
+
+	var (
+		sum1 = 100 + 150
+		sum2 = sum1 + 200
+		sum3 = sum2 + 500
+	)
+	fmt.Println(sum1)
+	fmt.Println(sum2)
+	fmt.Println(sum3)
 }
