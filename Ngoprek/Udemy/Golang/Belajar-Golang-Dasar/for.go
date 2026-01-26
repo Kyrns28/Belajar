48351f64e00d545314a0e8e1d2073a940de2f67a
Rizki Nur Sepriana <rizkinursepriana@gmail.com>
2026-01-26 20:39:25 +0700

    update

--- Ngoprek/Udemy/Golang/Belajar-Golang-Dasar/for.go
+++ Ngoprek/Udemy/Golang/Belajar-Golang-Dasar/for.go
@@ -1,7 +1,8 @@
 package main
+
 import "fmt"
 
-func main(){
+func main() {
 	counter := 1
 
 	for counter <= 10 {
@@ -10,7 +11,26 @@ func main(){
 	}
 
 	// for dengan statement
-	for counter := 1; counter <=10; counter++ {
+	for counter := 1; counter <= 10; counter++ {
 		fmt.Println("Ini perulangan yang ke ", counter)
+	}
+
+	fmt.Println("Selesai")
+	fmt.Println()
+
+	names := []string{"Rizki", "Nur", "Sepriana"}
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[i])
+	}
+	fmt.Println()
+	// for range dengan index
+	for index, name := range names {
+		fmt.Println("Index", index, "=", name)
+	}
+	fmt.Println()
+
+	// for range tanpa index
+	for _, name := range names {
+		fmt.Println(name)
 	}
 }
