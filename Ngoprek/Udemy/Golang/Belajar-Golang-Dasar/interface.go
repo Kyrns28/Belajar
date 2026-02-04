b136bdcb6e5217090a245277363bbbf1763e58a9
Rizki Nur Sepriana <rizkinursepriana@gmail.com>
2026-02-04 07:00:03 +0700

    update

--- Ngoprek/Udemy/Golang/Belajar-Golang-Dasar/interface.go
+++ Ngoprek/Udemy/Golang/Belajar-Golang-Dasar/interface.go
@@ -6,12 +6,35 @@ type HasName interface {
 	GetName() string
 }
 
-func SayHello(value HasName) {
-	fmt.Println("Hello", value.GetName())
+func sayHello(value HasName) {
+	fmt.Println("Helloww", value.GetName())
 }
 
+// membuat struct
 type Person struct {
 	Name string
 }
 
-func
+// Membuat Method
+func (person Person) GetName() string {
+	return person.Name
+}
+
+// Membuat struct
+type Animal struct {
+	Name string
+}
+
+// Membuat method
+func (animal Animal) GetName() string {
+	return animal.Name
+}
+
+func main() {
+
+	person := Person{Name: "Rizki"}
+	sayHello(person)
+
+	animal := Animal{Name: "Kuda"}
+	sayHello(animal)
+}
