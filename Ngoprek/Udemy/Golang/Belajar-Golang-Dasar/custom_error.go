ddcbc6de83d9956d7b2a1302a27446187022e267
Kyrns28 <123502414+Kyrns28@users.noreply.github.com>
2026-02-15 15:14:41 +0700

    update

---
+++ Ngoprek/Udemy/Golang/Belajar-Golang-Dasar/custom_error.go
@@ -0,0 +1,48 @@
+package main
+import "fmt"
+
+type validationError struct {
+	Message string
+}
+
+func(v *validationError) Error() string {
+	return v.Message
+}
+
+type notFoundError struct {
+	Message string
+}
+
+func(n *notFoundError) Error() string {
+	return n.Message
+}
+
+
+func SaveData(id string, data any) error {
+	if id == "" {
+		return &validationError{"validation Error"}
+	}
+	if id != "Kinur" {
+		return &notFoundError{"Data Not found "}
+	}
+
+	// OK
+	return nil
+}
+
+
+func main() {
+	err := SaveData("Kinur", nil)
+	if err != nil{
+		// terjadi error
+		if validationErr, ok := err.(*validationError); ok{
+			fmt.Println("Validation error", validationErr.Error())
+		} else if notFoundErr, ok := err.(*notFoundError); ok{
+			fmt.Println("Not found error", notFoundErr.Error())
+		} else {
+			fmt.Println("Unknown error", err.Error())
+		}
+	} else {
+		fmt.Println("Sukses")
+	}
+}
