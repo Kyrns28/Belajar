6410728af41f26fad9d70bfe2ef68f553d4ba9ad
Kyrns28 <123502414+Kyrns28@users.noreply.github.com>
2026-02-15 14:01:32 +0700

    update

--- Ngoprek/Udemy/Golang/Belajar-Golang-Dasar/import.go
+++ Ngoprek/Udemy/Golang/Belajar-Golang-Dasar/import.go
@@ -7,4 +7,10 @@ import (
 func main(){
 	result := helper.SayHello("Kinur")
 	fmt.Println(result)
+
+	// kode normal
+	fmt.Println(helper.SayHello("Rizkinur"))
+
+	// tidak dapat memanggil func nya karena huruf kecil diawal dan diluar package
+	fmt.Println(helper.sayGoodbye("Rizkinur"))
 }
